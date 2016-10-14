27d7294e2a8a7441f4727d4e64718c96ccd2a7cb
Annebelle Olminkhof <annebelleolminkhof@gmail.com>
2016-10-14 04:51:07 +0000

    Done.

--- lib/recycle.rb
+++ lib/recycle.rb
@@ -18,40 +18,40 @@ first_recycle_bin =  {
 #1. Define a method `address`, that takes a hash as an argument.
 #This method should return the value of the address key in the hash.
 
-# def address(hash)
-#   hash.first
-# end
+def address(hash)
+  hash["address"]
+end
 
-# puts address(first_recycle_bin)
+puts address(first_recycle_bin)
 
 #2. Define a method called `borough` that takes a hash as an argument.
 # This method should return the value of the borough key in the hash.
 
-# def borough(hash)
-#   hash["borough"]
-# end
+def borough(hash)
+  hash["borough"]
+end
 
-# puts borough(first_recycle_bin)
+puts borough(first_recycle_bin)
 
 #3. Define a method called `all_keys` that takes a hash as an argument.
 #This method should print out each key in the hash.
 #You should use the `each_key` method to iterate over your hash and print out each key.
 
-# def all_keys(hash)
-#   hash.each_key{|key| puts key}
-# end
+def all_keys(hash)
+  hash.each_key{|key| puts key}
+end
 
-# all_keys(first_recycle_bin)
+all_keys(first_recycle_bin)
 
 #4. Define a method called `all_data` that takes a hash as an argument.
 #This method should print out each value in the hash.
 #You should use the `each_value` method to iterate over your hash and print out each value.
 
-# def all_data(hash)
-#   hash.each_value{|value| puts value}
-# end
+def all_data(hash)
+  hash.each_value{|value| puts value}
+end
 
-# all_data(first_recycle_bin)
+all_data(first_recycle_bin)
 
 #5. Define a method called `key_and_data` that takes a hash as an argument.
 #This method should print out each key and value in the hash in a sentence like this: `The #{key} is #{value}`
@@ -59,7 +59,7 @@ first_recycle_bin =  {
 
 def key_and_data(hash)
   hash.each do |key, value|
-  puts "Your #{key} is #{value}."
+  puts "The #{key} is #{value}"
   end
 end
 
